155fbca15e4660b88907027da4c2d1b592752979
elmerneo <132624093+elmerneo@users.noreply.github.com>
2024-08-17 17:14:18 -0600

    Add files via upload

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'umg_antigua_2024',
+  username: 'umg_antigua_2024_user',
+  password: '8cIyQNzVx9MJIuf0LcIsLRcskSTPplzv',
+  host: 'dpg-cr0idci3esus73aiuu3g-a',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
